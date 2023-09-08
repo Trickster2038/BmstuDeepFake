6d1e5c04a139cf0da1e8d00f332db266b9ebf070
Sergey <33185046+Trickster2038@users.noreply.github.com>
2023-09-09 00:48:09 +0300

    ibug 300 regions hists

--- faceparts.py
+++ faceparts.py
@@ -6,72 +6,89 @@ import imutils
 import dlib
 import numpy as np
 
+LEFT_EAR_POINT = 1
+RIGHT_EAR_POINT = 17
+
+HIST_SCAN_POINTS = {
+    'r_eye_inner': 43, 
+    'r_eye_outer': 46, 
+    'l_mouth_outer': 49,
+    'center_mouth_top': 63,
+    # 'r_brow_inner': 23,
+    # 'l_brow_outer': 18,
+    # 'r_ear': 17,
+    # 'l_chin': 5,
+    # 'center_chin': 9,
+    # 'forehead': 28,
+    # 'l_nose': 32
+    }
+
 PREDICTOR_DATASET = './shape_predictor_68_face_landmarks.dat'
 PREDICTOR = dlib.shape_predictor(PREDICTOR_DATASET)
 COUNTOUR_COLOR = (0, 0, 255)
 # define a dictionary that maps the indexes of the facial
 # landmarks to specific face regions
 FACIAL_LANDMARKS_IDXS = collections.OrderedDict([
-	("mouth", (48, 68)),
-	("right_eyebrow", (17, 22)),
-	("left_eyebrow", (22, 27)),
-	("right_eye", (36, 42)),
-	("left_eye", (42, 48)),
-	("nose", (27, 35)),
-	("jaw", (0, 17))
+    ("mouth", (48, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 35)),
+    ("jaw", (0, 17))
 ])
 
 def visualize_facial_landmarks(image, shape, colors=None, alpha=0.75):
-	# create two copies of the input image -- one for the
-	# overlay and one for the final output image
-	overlay = image.copy()
-	output = image.copy()
-	# if the colors list is None, initialize it with a unique
-	# color for each facial landmark region
-	if colors is None:
-		colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
-			(168, 100, 168), (158, 163, 32),
-			(163, 38, 32), (180, 42, 220)]
-		
-	# loop over the facial landmark regions individually
-	for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
-		# grab the (x, y)-coordinates associated with the
-		# face landmark
-		(j, k) = FACIAL_LANDMARKS_IDXS[name]
-		pts = shape[j:k]
-		# check if are supposed to draw the jawline
-		if name == "jaw":
-			# since the jawline is a non-enclosed facial region,
-			# just draw lines between the (x, y)-coordinates
-			for l in range(1, len(pts)):
-				ptA = tuple(pts[l - 1])
-				ptB = tuple(pts[l])
-				cv.line(overlay, ptA, ptB, colors[i], 2)
-		# otherwise, compute the convex hull of the facial
-		# landmark coordinates points and display it
-		else:
-			pass
-			hull = cv.convexHull(pts)
-			cv.drawContours(overlay, [hull], -1, colors[i], -1)
-			
+    # create two copies of the input image -- one for the
+    # overlay and one for the final output image
+    overlay = image.copy()
+    output = image.copy()
+    # if the colors list is None, initialize it with a unique
+    # color for each facial landmark region
+    if colors is None:
+        colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
+            (168, 100, 168), (158, 163, 32),
+            (163, 38, 32), (180, 42, 220)]
+        
+    # loop over the facial landmark regions individually
+    for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
+        # grab the (x, y)-coordinates associated with the
+        # face landmark
+        (j, k) = FACIAL_LANDMARKS_IDXS[name]
+        pts = shape[j:k]
+        # check if are supposed to draw the jawline
+        if name == "jaw":
+            # since the jawline is a non-enclosed facial region,
+            # just draw lines between the (x, y)-coordinates
+            for l in range(1, len(pts)):
+                ptA = tuple(pts[l - 1])
+                ptB = tuple(pts[l])
+                cv.line(overlay, ptA, ptB, colors[i], 2)
+        # otherwise, compute the convex hull of the facial
+        # landmark coordinates points and display it
+        else:
+            pass
+            hull = cv.convexHull(pts)
+            cv.drawContours(overlay, [hull], -1, colors[i], -1)
+            
     # apply the transparent overlay
-	cv.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
-	# return the output image
-	return output
+    cv.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+    # return the output image
+    return output
 
 def detect_faces(image):
-	
+    
     # initialize dlib's face detector (HOG-based) and then create
     # the facial landmark predictor
     detector = dlib.get_frontal_face_detector()
     predictor = PREDICTOR
-	
+    
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     # detect faces in the grayscale image
     rects = detector(gray, 1)
-	
+    
     shapes = []
-	
+    
     # loop over the face detections
     for (i, rect) in enumerate(rects):
         # determine the facial landmarks for the face region, then
@@ -80,6 +97,7 @@ def detect_faces(image):
         shape = face_utils.shape_to_np(shape)
         # loop over the face parts individually
         for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+            '''
             # clone the original image so we can draw on it, then
             # display the name of the face part on the image
             clone = image.copy()
@@ -89,14 +107,15 @@ def detect_faces(image):
             # specific face part
             for (x, y) in shape[i:j]:
                 cv.circle(clone, (x, y), 1, (0, 0, 255), -1)
-				
-            # extract the ROI of the face region as a separate image
             '''
+                
+            # extract the ROI of the face region as a separate image
+            
             (x, y, w, h) = cv.boundingRect(np.array([shape[i:j]]))
             roi = image[y:y + h, x:x + w]
-            roi = imutils.resize(roi, width=250, inter=cv.INTER_CUBIC)
-			'''
-			
+            # roi = imutils.resize(roi, width=250, inter=cv.INTER_CUBIC)
+            
+            
             shapes.append(shape)
 
     return shapes
--- main.py
+++ main.py
@@ -2,22 +2,32 @@ import cv2 as cv
 import time
 import numpy as np
 import faceparts
+from matplotlib import pyplot as plt
 
 print('===== Init =====')
 
+plt.ion()
+
 FILE_PATH = '../ml_fake_videos/fake_freeman_1080.mp4'
-SCALE_PERCENT = 30
+# SCALE_PERCENT = 30
+MAX_WIDTH = 720
+MAX_HEIGHT = 480
 REAL_TIME_DELAY_24_FPS = 1.0 / 24.0
+DELAY = REAL_TIME_DELAY_24_FPS * 24 * 10
 FPS_LABEL_BORDER_PERCENT = 10 
 
 FACE_DETECTION_SIZE_PERCENT = 20
-WAIT_KEY = 1 # 0 - play pressing any key, 1 - autoplay
+WAIT_KEY = 0 # 0 - play pressing any key, 1 - autoplay
 
 TEXT_COLOR = (0,255,0)
 TEXT_SCALE = 3
 TEXT_THICKNESS = 2
 TEXT_FONT = cv.FONT_HERSHEY_SIMPLEX
 
+HIST_SIZE_PERCENT = 100 # relative to face width
+
+global fig, axs
+fig, axs = plt.subplots(len(faceparts.HIST_SCAN_POINTS), 1)
 
 face_classifier = cv.CascadeClassifier(
     cv.data.haarcascades + "haarcascade_frontalface_default.xml"
@@ -28,10 +38,14 @@ frame_cnt = 0
 
 # ===== MAIN =====
 while vd.isOpened():
+    print(f'~ prepare frame #{frame_cnt}')
     ret, frame = vd.read()
     frame_cnt += 1
     
-    frame_to_show = frame
+    frame_to_show = frame.copy()
+    frame_to_show_2 = frame.copy()
+
+    gray_frame = cv.cvtColor(frame.copy(), cv.COLOR_BGR2GRAY)
 
     # count frames
     text = str(frame_cnt)
@@ -50,39 +64,111 @@ while vd.isOpened():
         TEXT_THICKNESS, 
         cv.LINE_AA)
 
+    print(f'~ face recognition type 1 #{frame_cnt}')
     # face recognition
-
-    gray_image = cv.cvtColor(frame_to_show, cv.COLOR_BGR2GRAY)
     # TODO: check params (1.1, 5 - default)
-    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, 
+    faces = face_classifier.detectMultiScale(gray_frame, 1.1, 5, 
                                              minSize=(
                                                  int(frame_to_show.shape[0] * FACE_DETECTION_SIZE_PERCENT / 100.0), 
                                                  int(frame_to_show.shape[0] * FACE_DETECTION_SIZE_PERCENT / 100.0)))
     for (x, y, w, h) in faces:
         cv.rectangle(frame_to_show, (x, y), (x + w, y + h), TEXT_COLOR, TEXT_SCALE)
-    gray_image = cv.cvtColor(gray_image, cv.COLOR_GRAY2BGR)
+    # TODO: NOTICE gray -> colored
+    # gray_frame_colored = cv.cvtColor(gray_image, cv.COLOR_GRAY2BGR)
 
+    print(f'~ face recognition type 2 #{frame_cnt}')
     # detect faceparts
     shapes = faceparts.detect_faces(frame_to_show)
-    frame_to_show = faceparts.visualize_facial_landmarks(
-        frame_to_show, 
-        shapes[0]
-        )
+    if len(shapes) > 0:
+        shp = shapes[0]
+        frame_to_show = faceparts.visualize_facial_landmarks(
+            frame_to_show, 
+            shp
+            )
+    
+        # scan for hists
+        r = pow(
+            pow(shp[faceparts.LEFT_EAR_POINT][0] - shp[faceparts.RIGHT_EAR_POINT][0], 2) +  
+            pow(shp[faceparts.LEFT_EAR_POINT][1] - shp[faceparts.RIGHT_EAR_POINT][1], 2), 
+            0.5
+            )
+        rect_size = int((r * HIST_SIZE_PERCENT) / 100.0)
+
+        hists_data = []
+        for key in faceparts.HIST_SCAN_POINTS.keys():
+            point = shp[faceparts.HIST_SCAN_POINTS[key]]
+            point_x = point[0]
+            point_y = point[1]
+            x1, y1, x2, y2 = \
+                int(point_x - int(rect_size/2)), \
+                int(point_y - int(rect_size/2)), \
+                int(point_x + int(rect_size/2)), \
+                int(point_y + int(rect_size/2))
+                
+            # print(">>",int(rect_size/2),x1,x2,y1,y2,(x1-x2),(y1-y2))
+            # FIXME: zero-safeness for coords
+            cv.rectangle(
+                frame_to_show_2, 
+                (x1, y1), 
+                (x2, y2), 
+                TEXT_COLOR, 
+                TEXT_SCALE
+                )
+            
+            # TODO: normalized hist or not?
+
+            # counts, bins = np.histogram(x)
+            # plt.stairs(counts, bins)
+            hists_data.append({'area': [(x1, y1), (x2, y2)], 'label': key})
+
+        print(f'~ hists for face recognition type 2 #{frame_cnt}')
+        face_parts_frames = []
+        for i in range(len(hists_data)):
+            data = hists_data[i]
+            # print(data)
+            x1 = data['area'][0][0]
+            y1 = data['area'][0][1]
+            x2 = data['area'][1][0]
+            y2 = data['area'][1][1]
+            axs[i].set_title(data['label'] + '_' + str(frame_cnt))
+            axs[i].hist(gray_frame[y1:y2, x1:x2].ravel(),256,[0,256])
+            face_parts_frames.append(
+                cv.cvtColor(gray_frame[y1:y2, x1:x2], cv.COLOR_GRAY2BGR)
+                )
+        fig.canvas.draw()
+        fig.canvas.flush_events()   
+
+    print(f'~ render #{frame_cnt}')
 
     # resize
-    width = int(frame_to_show.shape[1] * SCALE_PERCENT / 100.0)
-    height = int(frame_to_show.shape[0] * SCALE_PERCENT / 100.0)
+    k = max(frame_to_show.shape[0] / MAX_HEIGHT, frame_to_show.shape[1] / MAX_WIDTH)
+    width = int(frame_to_show.shape[1] / k)
+    height = int(frame_to_show.shape[0] / k)
     dim = (width, height)
     frame_to_show = cv.resize(frame_to_show, dim, interpolation = cv.INTER_AREA)
-    gray_image = cv.resize(gray_image, dim, interpolation = cv.INTER_AREA)
+    frame_to_show_2 = cv.resize(frame_to_show_2, dim, interpolation = cv.INTER_AREA)
 
     # show
-    multi_frames = np.concatenate((frame_to_show, gray_image), axis=1)
+    face_parts_frames_united = np.concatenate(face_parts_frames, axis=1)
+    multi_frames = np.concatenate((frame_to_show, frame_to_show_2), axis=1)
+    face_parts_frames_united = cv.resize(
+        face_parts_frames_united, 
+        (
+            #       x1 / y1 = x2 / y2
+            # =>    y2 = x2 * y1 / x1
+            multi_frames.shape[1],
+            int(float(multi_frames.shape[1]) * face_parts_frames_united.shape[0] / face_parts_frames_united.shape[1]),
+        ), 
+        interpolation = cv.INTER_AREA)
+    multi_frames = np.concatenate((multi_frames, face_parts_frames_united), axis=0)
     cv.imshow('Play', multi_frames)
+
+    print(f'~ waiting key #{frame_cnt}')
     if WAIT_KEY == 1:
-        time.sleep(REAL_TIME_DELAY_24_FPS * 4)
+        time.sleep(DELAY)
     if cv.waitKey(WAIT_KEY) == ord('q'):
-        break
+        plt.pause(0.1) 
+        break 
 
 vd.release()
 cv.destroyAllWindows()
