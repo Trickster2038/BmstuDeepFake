2fb4e7a28e4310e9a54ba275dc894f3e491b5a01
Sergey <33185046+Trickster2038@users.noreply.github.com>
2023-09-19 23:37:03 +0300

    DECISION TREE MODEL

--- datacollector.py
+++ datacollector.py
@@ -188,11 +188,11 @@ def generate_shape(frame, filename, is_fake, frame_n, face_classifier):
 SAVEABLE = True
 SAVE_PATH = '../dataset_frames/'
 SAVE_FAIL_PATH = '../dataset_frames_fail/'
-FOLDER_PATH = '../dfdc_train_part_1/'
-METADATA_PATH = '../dfdc_train_part_1/metadata.json'
+FOLDER_PATH = '../dfdc_train_part_3/'
+METADATA_PATH = '../dfdc_train_part_3/metadata.json'
 TARGET_FRAME_N = 30
 SAMPLE_SIZE = 0 # 0 - no limit
-OUTPUT_FILENAME = '../dataframe_total_1.json'
+OUTPUT_FILENAME = '../dataframe_total_3.json'
 
 if __name__ == "__main__":
 
