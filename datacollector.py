ce18fcdce808fc28edbf9216274ab6b9cedda11e
Sergey <33185046+Trickster2038@users.noreply.github.com>
2023-09-20 13:07:41 +0300

    big dataset notebook

--- datacollector.py
+++ datacollector.py
@@ -188,11 +188,11 @@ def generate_shape(frame, filename, is_fake, frame_n, face_classifier):
 SAVEABLE = True
 SAVE_PATH = '../dataset_frames/'
 SAVE_FAIL_PATH = '../dataset_frames_fail/'
-FOLDER_PATH = '../dfdc_train_part_3/'
-METADATA_PATH = '../dfdc_train_part_3/metadata.json'
+FOLDER_PATH = '../dfdc_videos/dfdc_train_part_4/'
+METADATA_PATH = '../dfdc_videos/dfdc_train_part_4/metadata.json'
 TARGET_FRAME_N = 30
 SAMPLE_SIZE = 0 # 0 - no limit
-OUTPUT_FILENAME = '../dataframe_total_3.json'
+OUTPUT_FILENAME = '../dfdc_dataframes/dataframe_total_4.json'
 
 if __name__ == "__main__":
 
